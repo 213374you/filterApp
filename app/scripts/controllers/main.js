ad05bfdfde95ec65205d74b28119f0b62a092f87
ark <tobrien820@gmail.com>
2013-11-08 15:40:57 -0500

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('johnUIApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Hello John',
+      'test'
+    ];
+  }]);
